67980d48edefe0a881a4dc6f6fb8c36124b000d6
Trustacean <pizzacrustacean15@gmail.com>
2026-03-26 13:38:18 +0700

    readability improvement

--- src/main/java/org/trustacean/pubsubqe/util/DatasetLoader.java
+++ src/main/java/org/trustacean/pubsubqe/util/DatasetLoader.java
@@ -1,7 +1,6 @@
 package org.trustacean.pubsubqe.util;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,32 +8,27 @@ import org.trustacean.pubsubqe.core.Message;
 
 public class DatasetLoader {
 
-    private static final String DEFAULT_FILE_NAME = "src\\main\\java\\org\\trustacean\\pubsubqe\\input\\dataset.csv";
-    // private static final String DEFAULT_CONTEXT_FILE_NAME = "../input/context.csv";
+    private static final String PATH = "./src/main/java/org/trustacean/pubsubqe/input/";
+    private static final String DEFAULT_FILE_NAME = "dataset.csv";
+    private static final String DEFAULT_CONTEXT_FILE_NAME = "context.csv";
 
     public static List<Message> load() throws Exception {
-        return load(DEFAULT_FILE_NAME);
+        return load(DEFAULT_FILE_NAME, DEFAULT_CONTEXT_FILE_NAME);
     }
 
-    public static List<Message> load(String filename) throws Exception {
+    public static List<Message> load(String textDataset, String contextDataset) throws Exception {
         List<Message> list = new ArrayList<>();
+        try (BufferedReader brText = new BufferedReader(new java.io.FileReader(PATH + textDataset)); BufferedReader brContext = new BufferedReader(new java.io.FileReader(PATH + contextDataset))) {
+            brText.readLine();
 
-        File file = new File(filename);
-        if (file.exists()) {
-            System.out.println("File Exists");
-        } else {
-            System.out.println("File does not Exist.");
-        }
-
-        try (BufferedReader br = new BufferedReader(new java.io.FileReader(filename))) {
-            br.readLine();
-            
-            String line;
-            while ((line = br.readLine()) != null) {
-                list.add(new Message("", line));
+            String textLine;
+            String contextLine;
+            while ((textLine = brText.readLine()) != null && (contextLine = brContext.readLine()) != null) {
+                list.add(new Message(contextLine, textLine));
             }
+        } catch (Error e) {
+            System.out.println(e);
         }
-
         return list;
     }
 }
