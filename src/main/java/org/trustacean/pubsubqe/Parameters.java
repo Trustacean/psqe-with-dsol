19c759b9e3df0cae2b7b0d0f8eded073e9e25f22
Trustacean <pizzacrustacean15@gmail.com>
2026-05-22 13:44:28 +0700

    feat: add Parameters class with configuration constants

---
+++ src/main/java/org/trustacean/pubsubqe/Parameters.java
@@ -0,0 +1,9 @@
+package org.trustacean.pubsubqe;
+
+public class Parameters {
+    public static final int TOP_K = 8;
+    public static final int WINDOW_SIZE = 5000;
+    public static final int RECOMPUTE_INTERVAL = 500;
+    public static final double EXPANSION_THRESHOLD = 0.08;
+    public static final double RELEVANCE_THRESHOLD = 0.5;
+}
