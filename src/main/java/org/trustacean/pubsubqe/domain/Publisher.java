e09642f5d8792c887caf9f0857ffb15dc7278962
Trustacean <pizzacrustacean15@gmail.com>
2026-05-22 13:45:46 +0700

    feat: normalize message text before publishing

--- src/main/java/org/trustacean/pubsubqe/domain/Publisher.java
+++ src/main/java/org/trustacean/pubsubqe/domain/Publisher.java
@@ -2,10 +2,13 @@ package org.trustacean.pubsubqe.domain;
 
 import org.djutils.event.Event;
 import org.djutils.event.LocalEventProducer;
+import org.trustacean.pubsubqe.domain.gtrm.GlobalTermRelationshipModel;
 
 public class Publisher extends LocalEventProducer {
 
     public void publish(Message msg) {
+        String normalizedText = String.join(" ", GlobalTermRelationshipModel.normalize(msg.getText()));
+        msg.setText(normalizedText);
         // System.out.println("Publisher sent: " + msg.getText());
         fireEvent(new Event(Message.MESSAGE_PUBLISHED_EVENT, msg));
     }
