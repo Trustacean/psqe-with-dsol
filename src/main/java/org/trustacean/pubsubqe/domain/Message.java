ea1b67d21002fccdd4bde9ca28444e70c1b0e52b
Trustacean <pizzacrustacean15@gmail.com>
2026-05-23 12:15:43 +0700

    feat: add tokenization functionality to Message class for improved text processing

--- src/main/java/org/trustacean/pubsubqe/domain/Message.java
+++ src/main/java/org/trustacean/pubsubqe/domain/Message.java
@@ -1,5 +1,9 @@
 package org.trustacean.pubsubqe.domain;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.djutils.event.EventType;
 import org.djutils.metadata.MetaData;
 import org.djutils.metadata.ObjectDescriptor;
@@ -7,11 +11,11 @@ import org.djutils.metadata.ObjectDescriptor;
 public class Message {
 
     private final String context;
+    private Set<String> tokenizedWords;
     private String text;
-    public static final EventType MESSAGE_PUBLISHED_EVENT
-            = new EventType("MESSAGE_PUBLISHED_EVENT",
-                    new MetaData("MESSAGE_PUBLISHED_EVENT", "dataset message event",
-                            new ObjectDescriptor("Text", "Text data", Message.class)));
+    public static final EventType MESSAGE_PUBLISHED_EVENT = new EventType("MESSAGE_PUBLISHED_EVENT",
+            new MetaData("MESSAGE_PUBLISHED_EVENT", "dataset message event",
+                    new ObjectDescriptor("Text", "Text data", Message.class)));
     public static final EventType MESSAGE_DELIVERED_EVENT = new EventType("MESSAGE_DELIVERED_EVENT",
             new MetaData("MESSAGE_DELIVERED_EVENT", "message delivered event",
                     new ObjectDescriptor("Text", "Text data", Subscriber.class)));
@@ -19,10 +23,14 @@ public class Message {
     public Message(String Context, String text) {
         this.context = Context;
         this.text = text;
+        this.tokenizedWords = Arrays.stream(this.text.split("\\s+"))
+                .collect(Collectors.toSet());
     }
 
     public void setText(String text) {
         this.text = text;
+        this.tokenizedWords = Arrays.stream(this.text.split("\\s+"))
+                .collect(Collectors.toSet());
     }
 
     public String getContext() {
@@ -31,5 +39,9 @@ public class Message {
 
     public String getText() {
         return text;
+    }
+
+    public Set<String> getTokenizedWords() {
+        return this.tokenizedWords;
     }
 }
